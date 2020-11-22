8fb6606d6ea65dfb3a0690dc91953876673d6ef3
Jimloe <40555776+Jimloe@users.noreply.github.com>
2020-11-22 13:26:22 +0100

    Update ShturmanBOT.py

--- ShturmanBOT.py
+++ ShturmanBOT.py
@@ -5,9 +5,5 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config')
 
-# fhb = flairhelperbot.FlairHelperBot('EFTDesign')  # Create the FHB object to do stuff with
-# asyncio.run(fhb.run(True))  # Create an Asyncio task to run the FHB object
-
 # Run our Discord Bot from the python script we've created
 DiscordBot.client.run(config['DISCORD']['token'])
-
